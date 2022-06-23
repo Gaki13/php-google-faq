bb231b5a45c500ebb0b591ffde7fe4da3fb2d47a
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-23 15:51:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// Ci sono diverse domande con relative risposte.
+// Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. 
+$faqs= [
+
+]
+ 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
